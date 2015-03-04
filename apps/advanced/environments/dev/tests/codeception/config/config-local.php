6bd34e5302ba5c487f908bef3ece9222db7c8c4c
Vojtech Horak <vojtech.horak@gmail.com>
2015-03-04 01:20:07 +0100

    Isolated local config files used by test
    - YML configs used by codeception test are moved to the Environments and
    marked as untracked for the repository.
    - New config-local.php is created to hold local params (DB connection
    etc.)

---
+++ apps/advanced/environments/dev/tests/codeception/config/config-local.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Local application configuration shared by all applications and test types
+ */
+return [
+    'components' => [
+        'db' => [
+            'dsn' => 'mysql:host=localhost;dbname=yii2_advanced_tests',
+        ],
+    ],
+];
